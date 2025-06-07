274a7938b73068ec534f96dd50d6366e2e8b88c1
Jordan Ayotte <jordanayotte13@outlook.com>
2025-06-07 10:15:52 -0400

    added has_more_commands and advance routines

--- app.py
+++ app.py
@@ -3,17 +3,36 @@
 class Parser:
     def __init__(self, filename):
         with open(filename, "r") as file:
+            # open file and extract all needed lines from file
             arr = []
             for line in file:
+                # remove comment lines
                 if line.strip().startswith("/"):
                     continue
+                # remove blank lines
                 if not line.strip():
                     continue
                 arr.append(line.strip())
             self.lines = arr
+            # initialize variables needed to step through lines
+            self.current_command: str = None
+            self.command_counter: int = 0
+    
+    def has_more_commands(self):
+        if self.command_counter >= len(self.lines):
+            return False
+        return True
+    
+    def advance(self):
+        if self.has_more_commands:
+            self.current_command = self.lines[self.command_counter]
+            self.command_counter += 1
+        
+
     def print_lines(self):
         for line in self.lines:
-            print(line)
+            self.advance()
+            print(self.current_command)
 
 obj = Parser("test.asm")
 obj.print_lines()
